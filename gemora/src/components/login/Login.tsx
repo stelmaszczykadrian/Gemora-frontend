53b882d1761984da7fe721cba9a82d1229c89f00
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-21 11:44:25 +0200

    Refactor: Optimized responsiveness for the Login component

--- gemora/src/components/login/Login.tsx
+++ gemora/src/components/login/Login.tsx
@@ -1,7 +1,8 @@
-import {ErrorMessage, Field, Form, Formik, FormikHelpers} from "formik";
-import * as Yup from "yup";
+import React from 'react';
+import {ErrorMessage, Field, Form, Formik, FormikHelpers} from 'formik';
+import * as Yup from 'yup';
 import axios from 'axios';
-import {useNavigate} from "react-router-dom";
+import {useNavigate} from 'react-router-dom';
 
 interface FormValues {
     email: string;
@@ -13,78 +14,85 @@ const Login = () => {
 
     const initialValues: FormValues = {
         email: '',
-        password: ''
+        password: '',
     };
 
     const validationSchema = Yup.object().shape({
         email: Yup.string().email('Invalid email').required('Email is required.'),
-        password: Yup.string().required('Password is required.')
+        password: Yup.string().required('Password is required.'),
     });
 
     const handleSubmit = (
         values: FormValues,
-        { setSubmitting }: FormikHelpers<FormValues>
+        {setSubmitting}: FormikHelpers<FormValues>
     ) => {
         console.log(values);
-        axios.post('http://localhost:8080/api/v1/auth/authenticate', values)
-            .then(response => {
-                // Handle the response from the backend if needed
+        axios
+            .post('http://localhost:8080/api/v1/auth/authenticate', values)
+            .then((response) => {
                 console.log('Response from backend:', response.data);
 
                 if (response.data.access_token) {
-                    localStorage.setItem("token", JSON.stringify(response.data));
+                    localStorage.setItem('token', JSON.stringify(response.data));
                 }
                 navigate('/');
                 setSubmitting(false);
                 return response.data;
-
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error('Error sending data to backend:', error);
                 setSubmitting(false);
             });
     };
 
     return (
-        <div className="col-md-12">
-            <div className="card card-container">
-                <img
-                    src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-                    alt="profile-img"
-                    className="profile-img-card"
-                />
-                <Formik
-                    initialValues={initialValues}
-                    validationSchema={validationSchema}
-                    onSubmit={handleSubmit}
-                >
-                    <Form>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <Field name="email" type="text" className="form-control" />
-                            <ErrorMessage
-                                name="email"
-                                component="div"
-                                className="alert alert-danger"
-                            />
-                        </div>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-6">
+                    <div className="card card-container">
+                        <img
+                            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                            alt="profile-img"
+                            className="profile-img-card"
+                        />
+                        <Formik
+                            initialValues={initialValues}
+                            validationSchema={validationSchema}
+                            onSubmit={handleSubmit}
+                        >
+                            <Form>
+                                <div className="form-group">
+                                    <label htmlFor="email">Email</label>
+                                    <Field name="email" type="text" className="form-control"/>
+                                    <ErrorMessage
+                                        name="email"
+                                        component="div"
+                                        className="alert alert-danger"
+                                    />
+                                </div>
 
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <Field name="password" type="password" className="form-control" />
-                            <ErrorMessage
-                                name="password"
-                                component="div"
-                                className="alert alert-danger"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-primary btn-block">
-                                <span>Login</span>
-                            </button>
-                        </div>
-                    </Form>
-                </Formik>
+                                <div className="form-group">
+                                    <label htmlFor="password">Password</label>
+                                    <Field
+                                        name="password"
+                                        type="password"
+                                        className="form-control"
+                                    />
+                                    <ErrorMessage
+                                        name="password"
+                                        component="div"
+                                        className="alert alert-danger"
+                                    />
+                                </div>
+                                <div className="form-group text-center mt-4">
+                                    <button type="submit" className="btn btn-primary btn-block">
+                                        <span>Login</span>
+                                    </button>
+                                </div>
+                            </Form>
+                        </Formik>
+                    </div>
+                </div>
             </div>
         </div>
     );
