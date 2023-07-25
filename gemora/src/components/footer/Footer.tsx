167c165e4370b99720d60c81a13d11c06326ef33
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-25 14:02:56 +0200

    Feature: Added Footer.css for styling component

--- gemora/src/components/footer/Footer.tsx
+++ gemora/src/components/footer/Footer.tsx
@@ -1,26 +1,26 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <footer className="bg-light py-5">
+        <footer className="footer-bg py-5">
             <Container>
                 <Row className="justify-content-center">
                     <Col xs={12} md={6} lg={4} className="text-center">
                         <h5>Gemora</h5>
                         <p>
-                            Discover exquisite, high-quality jewelry that adds sparkle and elegance to your style. Enhance your individuality with our unique accessories.
+                            Discover exquisite, high-quality jewelry that adds sparkle and elegance to your style.
+                            Enhance your individuality with our unique accessories.
                         </p>
                     </Col>
                     <Col xs={6} md={3} lg={2} className="text-center">
                         <h6>Gemora</h6>
                         <ul className="list-unstyled">
-                            <li><a href="/">Resources</a></li>
                             <li><a href="/">About Us</a></li>
                             <li><a href="/">Contact</a></li>
-                            <li><a href="/">Blog</a></li>
                         </ul>
                     </Col>
-                    <Col xs={6} md={3} lg={2} className="text-center">
+                    <Col xs={6} md={3} lg={2} className="text-right">
                         <h6>Help</h6>
                         <ul className="list-unstyled">
                             <li><a href="/">Support</a></li>
