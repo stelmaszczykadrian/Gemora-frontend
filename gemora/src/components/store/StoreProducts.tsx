8284797d0e00bb1f7844b7bc5604b8c318e8028a
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-11 14:06:15 +0200

    Feature: Added StoreProducts component with product rendering

---
+++ gemora/src/components/store/StoreProducts.tsx
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from "react";
+import CartItem from "../cart/CartItem";
+import './StoreProducts.css';
+import axios from "axios";
+
+const StoreProducts: React.FC = () => {
+    const [products, setProducts] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://localhost:8080/api/products");
+                setProducts(response.data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
+    }, []);
+    console.log(products)
+
+    return (
+            <div className="products-bg container mt-5">
+                <div className="row">
+                    {products.map((product) => (
+                        <div key={product.id} className="col-md-3 mb-1">
+                            <CartItem
+                                name={product.name}
+                                price={product.price}
+                                imageUrl={product.image}
+                                totalPrice={product.price}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+    );
+};
+
+export default StoreProducts;
