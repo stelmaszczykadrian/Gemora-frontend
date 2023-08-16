bb6b92ef676f107affed6c56d113ab75fcbaff13
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-16 09:54:14 +0200

    Feature: Created ProductApi fetch utility for handling API requests

---
+++ gemora/src/api/ProductApi.ts
@@ -0,0 +1,26 @@
+import {Product} from "../components/ProductInterface";
+import axios from "axios";
+
+export const fetchProductsFromApi = async (productIds: number[]): Promise<Product[]> => {
+    const products: Product[] = [];
+    for (const productId of productIds) {
+        try {
+            const response = await axios.get(`http://localhost:8080/api/products/${productId}`);
+            products.push(response.data);
+        } catch (error) {
+            console.error("Error while downloading product data:", error);
+        }
+    }
+    return products;
+};
+
+
+export const fetchProductDataFromApi = async () => {
+    try {
+        const response = await axios.get("http://localhost:8080/api/products");
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return [];
+    }
+};
