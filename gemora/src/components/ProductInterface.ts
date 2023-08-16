ecf46b20cdb7232d075f297143320c70524d900a
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-16 09:51:36 +0200

    Feature: Added ProductInterface for defining product data structure

---
+++ gemora/src/components/ProductInterface.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    manufacturer: string;
+    category: string;
+    price: number;
+    image: string;
+
+}
