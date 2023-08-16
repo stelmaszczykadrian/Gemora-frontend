19c03c3f6346e184752e6fab82c059c6cb2a6405
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-16 09:52:55 +0200

    Feature: Implemented "Add to cart" functionality in ProductDetails component

--- gemora/src/components/ProductDetails.tsx
+++ gemora/src/components/ProductDetails.tsx
@@ -1,25 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-
-interface Product {
-    name: string;
-    description: string;
-    manufacturer: string;
-    category: string;
-    price: number;
-    image: string;
-
-}
+import {AuthService} from "../api/AuthService";
+import {Product} from "./ProductInterface";
 
 function ProductDetails() {
     const { id } = useParams<{ id: string }>();
     const [product, setProduct] = useState<Product | null>(null);
+    const [cart, setCart] = useState<Product[]>([]);
+    const userEmail = AuthService.getUserEmailFromToken();
+
+
+    const addToCart = () => {
+        if (product) {
+            const tokenString = localStorage.getItem('token');
+            if (tokenString) {
+                const quantity = 1;
+                axios.post(`http://localhost:8080/api/cart/${userEmail}/${product.id}/${quantity}`)
+                    .then(response => {
+                        console.log('Product added to cart:', response.data);
+                        setCart([...cart, product]);
+                    })
+                    .catch(error => {
+                        console.error('Error adding product to cart:', error);
+                    });
+            }
+        }
+    };
+
 
     useEffect(() => {
         axios.get(`http://localhost:8080/api/products/${id}`)
             .then(response => {
-                console.log(response)
                 setProduct(response.data);
             })
             .catch(error => {
@@ -38,6 +50,7 @@ function ProductDetails() {
                     <p>Category: {product.category}</p>
                     <p>Price: {product.price}</p>
                     <img src={`data:image/jpeg;base64,${product.image}`} alt={product.name} width="300px" height="300px"/>
+                    <button onClick={addToCart}>Add to Cart</button>
                 </>
             )}
         </div>
