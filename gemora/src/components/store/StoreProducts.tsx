e96b822febced57879d68a800495f81cb631afcb
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-17 17:43:58 +0200

    Feature: Added redirection to  product page details from store products

--- gemora/src/components/store/StoreProducts.tsx
+++ gemora/src/components/store/StoreProducts.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import ProductCard from "../product/ProductCard";
 import './StoreProducts.css';
 import {fetchProductDataFromApi} from "../../api/ProductApi";
+import {Link} from "react-router-dom";
 
 const StoreProducts: React.FC = () => {
     const [products, setProducts] = useState<any[]>([]);
@@ -19,12 +20,14 @@ const StoreProducts: React.FC = () => {
                 <div className="row">
                     {products.map((product) => (
                         <div key={product.id} className="col-md-3 mb-1">
-                            <ProductCard
-                                name={product.name}
-                                price={product.price}
-                                imageUrl={product.image}
-                                // totalPrice={product.price}
-                            />
+                            <Link className="customLink" to={`/product/${product.id}`}>
+                                <ProductCard
+                                    name={product.name}
+                                    price={product.price}
+                                    imageUrl={product.image}
+                                    // totalPrice={product.price}
+                                />
+                            </Link>
                         </div>
                     ))}
                 </div>
