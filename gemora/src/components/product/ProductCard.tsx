55743d11d397f8c7cd6e2b4a55e8d6ec0aded2bf
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-17 14:11:35 +0200

    Feature: Formatting in total price display

--- gemora/src/components/product/ProductCard.tsx
+++ gemora/src/components/product/ProductCard.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ProductCardProps from "./ProductCardProps";
+import {formatPrice} from "../../utils/utils";
 
-const ProductCard: React.FC<ProductCardProps> = ({name, price, imageUrl,totalPrice}) => {
+
+const ProductCard: React.FC<ProductCardProps> = ({name, price, imageUrl}) => {
     return (
         <div className="card text-center mb-3">
             <img src={`data:image/jpeg;base64,${imageUrl}`} alt={name} className="card-img-top"
@@ -9,7 +11,8 @@ const ProductCard: React.FC<ProductCardProps> = ({name, price, imageUrl,totalPri
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
                 <p className="card-text">
-                    Total Price: ${totalPrice.toFixed(2)}
+                    {formatPrice(price)}
+
                 </p>
             </div>
         </div>
