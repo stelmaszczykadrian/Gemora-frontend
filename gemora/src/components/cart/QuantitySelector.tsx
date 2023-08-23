4cde83672275cf4d986945fe7a60aacf09271d52
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-23 13:47:45 +0200

    Refactor: Removed unused code in QuantitySelector Component

--- gemora/src/components/cart/QuantitySelector.tsx
+++ gemora/src/components/cart/QuantitySelector.tsx
@@ -1,57 +1,29 @@
-import React, {useContext, useState} from 'react';
-import './ShoppingCart.css'
-import axios from "axios";
-import UserContext from "../../context/UserContext";
-import {BaseUrl} from "../../constants/constants";
+import React from 'react';
+import './ShoppingCart.css';
 
 interface QuantitySelectorProps {
     quantity: number;
     productId: number;
 }
 
-const QuantitySelector: React.FC<QuantitySelectorProps> = ({quantity, productId }) => {
+const QuantitySelector: React.FC<QuantitySelectorProps> = ({quantity, productId}) => {
 
-    const [localQuantity, setLocalQuantity] = useState(quantity);
-    const { currentUser} = useContext(UserContext);
+    const handleDecrease = () => {
+    }
 
-    const handleIncrease = async () => {
-        const newQuantity = localQuantity + 1;
-        setLocalQuantity(newQuantity);
-        await updateQuantity(newQuantity);
-    };
-
-    const handleDecrease = async () => {
-        const newQuantity = localQuantity - 1;
-        if (newQuantity > 0) {
-            setLocalQuantity(newQuantity);
-            await updateQuantity(newQuantity);
-        }
-    };
-
-    const updateQuantity = async (newQuantity: number) => {
-        try {
-            await axios.post(`${BaseUrl}/api/cart/updateQuantity`, {
-                productId,
-                newQuantity,
-                userId: currentUser?.id
-            });
-
-        } catch (error) {
-            console.error('Error during quantity update:', error);
-        }
-    };
+    const handleIncrease = () => {
+    }
 
     return (
         <div className="d-flex align-items-center">
             <div className="cart-product-quantity">
                 <div className="cart-quantity-buttons-container">
                     <button className="cart-quantity-decrease" onClick={handleDecrease}>-</button>
-                    <div className="cart-quantity">{localQuantity}</div>
+                    <div className="cart-quantity">{quantity}</div>
                     <button className="cart-quantity-increase" onClick={handleIncrease}>+</button>
                 </div>
             </div>
         </div>
     );
 };
-
 export default QuantitySelector;
