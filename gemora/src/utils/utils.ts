a451c4702b554bf33597fcd81a6f714ea7f0d297
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-23 13:46:40 +0200

    Feature: Added groupBy function

--- gemora/src/utils/utils.ts
+++ gemora/src/utils/utils.ts
@@ -1,3 +1,14 @@
 export const formatPrice = (price: number) => {
     return `$${price.toFixed(2)}`;
 };
+
+
+
+export function groupBy<T>(arr: T[], fn: (item: T) => any) {
+    return arr.reduce<Record<string, T[]>>((prev, curr) => {
+        const groupKey = fn(curr);
+        const group = prev[groupKey] || [];
+        group.push(curr);
+        return { ...prev, [groupKey]: group };
+    }, {});
+}
