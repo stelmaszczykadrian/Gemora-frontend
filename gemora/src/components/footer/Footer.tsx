aba1baa5416a63563ea2a998a4635ad56ca7f09c
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-23 13:26:05 +0200

    Refactor: Restructure Footer Component

--- gemora/src/components/footer/Footer.tsx
+++ gemora/src/components/footer/Footer.tsx
@@ -1,38 +1,59 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import React from 'react';
 import './Footer.css'
 
-const Footer = () => {
-    return (
-        <footer className="footer-bg py-2">
-            <Container>
-                <Row className="justify-content-center">
-                    <Col xs={12} md={6} lg={4} className="text-center">
-                        <h5>Gemora</h5>
-                        <p>
-                            Discover exquisite, high-quality jewelry that adds sparkle and elegance to your style.
-                            Enhance your individuality with our unique accessories.
-                        </p>
-                    </Col>
-                    <Col xs={6} md={3} lg={2} className="text-center">
-                        <h6>Gemora</h6>
-                        <ul className="list-unstyled">
-                            <li><a href="/about">About Us</a></li>
-                            <li><a href="/contact">Contact</a></li>
-                        </ul>
-                    </Col>
-                    <Col xs={6} md={3} lg={2} className="text-right">
-                        <h6>Help</h6>
-                        <ul className="list-unstyled">
-                            <li><a href="/support">Support</a></li>
-                            <li><a href="/register">Sign Up</a></li>
-                            <li><a href="/login">Sign In</a></li>
-                        </ul>
-                    </Col>
-                </Row>
-                <p className="text-center mt-5">&copy; Gemora, 2023. All rights reserved.</p>
-            </Container>
-        </footer>
-    );
-};
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPhone, faEnvelope, faLocationDot} from '@fortawesome/free-solid-svg-icons';
 
+const Footer: React.FC = () => {
+    return (
+        <>
+            <div>
+                <div className="footer-container">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6 col-lg-5 col-12 footer-first-column">
+                                <h3 className="first-column-title">Gemora</h3>
+                                <p className="first-column-description">Discover exquisite, high-quality jewelry that adds
+                                    sparkle and elegance to your style. Enhance your individuality with our unique
+                                    accessories.</p>
+                            </div>
+                            <div className="col-md-6 col-lg-3 col-12 footer-second-column">
+                                <h5 className="second-column-title">Our Company</h5>
+                                <p className="footer-divider"></p>
+                                <ul className="footer-nav">
+                                    <li className="footer-nav-item">
+                                        <a className="footer-nav-link" href="/">Contact Us</a>
+                                    </li>
+                                    <li className="footer-nav-item">
+                                        <a className="footer-nav-link" href="/">About Us</a>
+                                    </li>
+                                    <li className="footer-nav-item">
+                                        <a className="footer-nav-link" href="/">Sign Up</a>
+                                    </li>
+                                    <li className="footer-nav-item">
+                                        <a className="footer-nav-link" href="/">Sign In</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-md-6 col-lg-4 col-12 footer-third-column">
+                                <h5 className="third-column-title">Store Information</h5>
+                                <p className="footer-divider"></p>
+                                <p><FontAwesomeIcon icon={faPhone} className="footer-icon"/> +92 3121324083</p>
+                                <p className="footer-email">
+                                    <FontAwesomeIcon icon={faEnvelope} className="footer-icon"/>{" "}
+                                    <a href="mailto:gemora@contact.pl" className="footer-nav-link">gemora@contact.pl</a>
+                                </p>
+                                <p><FontAwesomeIcon icon={faLocationDot} className="footer-icon"/> Warsaw, Europe</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-second-row">© Gemora, 2023. All rights reserved.</div>
+            </div>
+
+        </>
+    )
+}
 export default Footer;
+
