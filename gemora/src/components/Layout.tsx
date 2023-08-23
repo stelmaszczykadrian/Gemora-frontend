0274c4bfd20b975fdfe60b509c67d391da56bc12
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-23 13:44:11 +0200

    Feature: Updated Layout component with Newsletter

--- gemora/src/components/Layout.tsx
+++ gemora/src/components/Layout.tsx
@@ -3,6 +3,7 @@ import Footer from "./footer/Footer";
 import {ReactElement, JSXElementConstructor, ReactNode, ReactPortal} from "react";
 import TopBar from "./topbar/TopBar";
 import Header from "./header/Header";
+import Newsletter from "./newsletter/Newsletter";
 
 function Layout(props: { children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) {
 
@@ -12,6 +13,7 @@ function Layout(props: { children: string | number | boolean | ReactElement<any,
             <Header/>
             <Navbar/>
             <main>{props.children}</main>
+            <Newsletter/>
             <Footer/>
         </div>
     )
