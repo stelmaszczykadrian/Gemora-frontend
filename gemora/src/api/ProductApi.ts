bced27bf5c5fd6466e029a5b23fd5153f487ae1a
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-23 13:41:15 +0200

    Refactor: Remove unused Code for product fetching

--- gemora/src/api/ProductApi.ts
+++ gemora/src/api/ProductApi.ts
@@ -1,19 +1,5 @@
-import {Product} from "../components/product/ProductInterface";
 import axios from "axios";
 import {BaseUrl} from "../constants/constants";
-
-export const fetchProductsFromApi = async (productIds: number[]): Promise<Product[]> => {
-    const products: Product[] = [];
-    for (const productId of productIds) {
-        try {
-            const response = await axios.get(`${BaseUrl}/api/products/${productId}`);
-            products.push(response.data);
-        } catch (error) {
-            console.error("Error while downloading product data:", error);
-        }
-    }
-    return products;
-};
 
 
 export const fetchProductDataFromApi = async () => {
