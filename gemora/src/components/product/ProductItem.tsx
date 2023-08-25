40f11fbd75dbd4bfff2f38a2a9d3b512d73e83ad
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-25 12:05:08 +0200

    Feature: Changed name of the component and add styling

---
+++ gemora/src/components/product/ProductItem.tsx
@@ -0,0 +1,31 @@
+import "./ProductItem.css";
+import React from "react";
+
+interface ProductProps {
+    name: string;
+    price: number;
+    imageUrl: string;
+}
+
+const ProductItem: React.FC<ProductProps> = ({name, price, imageUrl}) => {
+
+    return (
+        <>
+            <div className="product-container">
+                <div className="product-header">
+                    <img
+                        src={`data:image/jpeg;base64,${imageUrl}`}
+                        alt={name}
+                        className="card-img-top product-header-img"
+                    />
+                </div>
+                <div className="product-details">
+                    <p>{name}</p>
+                    <p className="product-item-price ">{price}$</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default ProductItem;
