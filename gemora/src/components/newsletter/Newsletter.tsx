55cb68840caedceac948ac32d930f26975f1bb5e
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-08-25 18:02:05 +0200

    Feature: Updated Newsletter Component text and styling

--- gemora/src/components/newsletter/Newsletter.tsx
+++ gemora/src/components/newsletter/Newsletter.tsx
@@ -3,10 +3,10 @@ import './Newsletter.css';
 const Newsletter = () => {
     return (
         <div className="newsletter-container">
-            <h1 className="newsletter-title">Newsletter</h1>
-            <div className="newsletter-description">Get timely updates from your favorite products.</div>
+            <h1 className="newsletter-title">SUBSCRIBE TO OUR NEWSLETTER</h1>
+            <div className="newsletter-description">Sign up for our newsletter to receive the latest updates on new collections, and exclusive offers delivered directly to your email inbox.</div>
             <div className="newsletter-input-container">
-                <input className="newsletter-input" placeholder="Your email" />
+                <input className="newsletter-input" placeholder="Enter your email address" />
                 <button className="newsletter-button">
                     Sign Up
                 </button>
