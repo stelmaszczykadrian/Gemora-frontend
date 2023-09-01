a32ebdcf6a3f36ab7d0dc06d9a74fa1188565cba
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-09-01 16:22:37 +0200

    Feature: Added enums for Categories and SortTypes

--- gemora/src/constants/constants.ts
+++ gemora/src/constants/constants.ts
@@ -1,2 +1,19 @@
 export const ACCESS_TOKEN = 'token';
 export const BaseUrl = "http://localhost:8080";
+
+
+export enum Categories {
+    ENGAGEMENTS= 'ENGAGEMENTS',
+    EARRINGS = 'EARRINGS',
+    PENDANTS = 'PENDANTS',
+    RINGS = 'RINGS',
+    BRACELETS = 'BRACELETS',
+    GEMSTONES = 'GEMSTONES'
+
+}
+
+export enum SortType {
+    ASCENDING= 'ascending',
+    DESCENDING = 'descending',
+    NEWEST = 'newest',
+}
