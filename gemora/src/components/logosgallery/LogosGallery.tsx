fcb2f5a710a5c89951e87668bf49cc8c5149a113
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-19 15:42:19 +0200

    Refactor: Organize brand logos in LogosGallery component

--- gemora/src/components/logosgallery/LogosGallery.tsx
+++ gemora/src/components/logosgallery/LogosGallery.tsx
@@ -1,20 +1,26 @@
 import React from "react";
-import Logo1 from "../../assets/brand-logos/kingscrown.jpg";
-import Logo2 from "../../assets/brand-logos/crown.jpg";
-import Logo3 from "../../assets/brand-logos/berlians.jpg";
-import Logo4 from "../../assets/brand-logos/luxury.jpg";
-import Logo5 from "../../assets/brand-logos/wonder.jpg";
+import kingsCrownLogo from "../../assets/brand-logos/kingscrown.jpg";
+import crownLogo from "../../assets/brand-logos/crown.jpg";
+import berliansLogo from "../../assets/brand-logos/berlians.jpg";
+import luxuryLogo from "../../assets/brand-logos/luxury.jpg";
+import wonderLogo from "../../assets/brand-logos/wonder.jpg";
 import './LogosGallery.css';
+
+const logoData = [
+    { src: kingsCrownLogo, alt: "Kings Crown" },
+    { src: crownLogo, alt: "Crown" },
+    { src: berliansLogo, alt: "Berlians" },
+    { src: luxuryLogo, alt: "Luxury" },
+    { src: wonderLogo, alt: "Wonder" },
+];
 
 const LogosGallery = () => {
     return (
         <>
             <div className="logos-container">
-                <img src={Logo1} alt="logo" className="scale-element logo-image"/>
-                <img src={Logo2} alt="logo" className="scale-element logo-image"/>
-                <img src={Logo3} alt="logo" className="scale-element logo-image"/>
-                <img src={Logo4} alt="logo" className="scale-element logo-image"/>
-                <img src={Logo5} alt="logo" className="scale-element logo-image"/>
+                {logoData.map((logo, index) => (
+                    <img key={index} src={logo.src} alt={logo.alt} className="scale-element logo-image" />
+                ))}
             </div>
         </>
     );
