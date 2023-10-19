835f152c41345970c3ee83bb82b90af75d446863
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-19 13:28:10 +0200

    Feature: Create AuthApi and user registration and authentication API functions for better error handling

---
+++ gemora/src/api/AuthApi.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import {BaseUrl} from "../constants/constants";
+import {RegisterFormValues} from "../components/register/RegisterBox";
+import {LoginFormValues} from "../components/login/LoginBox";
+
+export const registerUser = async (values: RegisterFormValues) => {
+    try {
+        const response = await axios.post(`${BaseUrl}/api/v1/auth/register`, values);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const authenticateUser = async (values: LoginFormValues): Promise<LoginFormValues> => {
+    try {
+        const response = await axios.post<LoginFormValues>(`${BaseUrl}/api/v1/auth/authenticate`, values);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
