9dcdec2fe2f9eeb04807b860364838bc678b1eca
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-19 15:45:06 +0200

    Refactor: Simplify Footer component with structured data

--- gemora/src/components/footer/Footer.tsx
+++ gemora/src/components/footer/Footer.tsx
@@ -5,6 +5,20 @@ import gemoraLogo from '../../assets/GemoraLogo.jpg';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPhone, faEnvelope, faLocationDot} from '@fortawesome/free-solid-svg-icons';
 
+const companyInfo = {
+    name: 'Gemora',
+    year: 2023,
+    phone: '+92 3121324083',
+    email: 'gemora@contact.pl',
+    location: 'Warsaw, Europe',
+};
+
+const footerLinks = [
+    { text: 'Contact Us', url: '/contact' },
+    { text: 'Sign Up', url: '/register' },
+    { text: 'Sign In', url: '/login' },
+];
+
 const Footer: React.FC = () => {
     return (
         <>
@@ -24,32 +38,30 @@ const Footer: React.FC = () => {
                                 <h5 className="second-column-title">Our Company</h5>
                                 <p className="footer-divider"></p>
                                 <ul className="footer-nav">
-                                    <li className="footer-nav-item">
-                                        <a className="footer-nav-link" href="/contact">Contact Us</a>
-                                    </li>
-                                    <li className="footer-nav-item">
-                                        <a className="footer-nav-link" href="/register">Sign Up</a>
-                                    </li>
-                                    <li className="footer-nav-item">
-                                        <a className="footer-nav-link" href="/login">Sign In</a>
-                                    </li>
+                                    {footerLinks.map((link, index) => (
+                                        <li key={index} className="footer-nav-item">
+                                            <a className="footer-nav-link" href={link.url}>
+                                                {link.text}
+                                            </a>
+                                        </li>
+                                    ))}
                                 </ul>
                             </div>
                             <div className="col-md-6 col-lg-4 col-12 footer-third-column">
                                 <h5 className="third-column-title">Store Information</h5>
                                 <p className="footer-divider"></p>
-                                <p><FontAwesomeIcon icon={faPhone} className="footer-icon"/> +92 3121324083</p>
+                                <p><FontAwesomeIcon icon={faPhone} className="footer-icon"/> {companyInfo.phone}</p>
                                 <p className="footer-email">
                                     <FontAwesomeIcon icon={faEnvelope} className="footer-icon"/>{" "}
-                                    <a href="mailto:gemora@contact.pl" className="footer-nav-link">gemora@contact.pl</a>
+                                    <a href="mailto:gemora@contact.pl" className="footer-nav-link">{companyInfo.email}</a>
                                 </p>
-                                <p><FontAwesomeIcon icon={faLocationDot} className="footer-icon"/> Warsaw, Europe</p>
+                                <p><FontAwesomeIcon icon={faLocationDot} className="footer-icon"/> {companyInfo.location}</p>
 
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="footer-second-row">© Gemora, 2023. All rights reserved.</div>
+                <div className="footer-second-row">© {companyInfo.name}, {companyInfo.year}. All rights reserved.</div>
             </div>
 
         </>
