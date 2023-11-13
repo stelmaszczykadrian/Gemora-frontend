705c608d716afcf3e040cedf4e598ae66de95724
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-11-13 13:49:18 +0100

    GEM-68 Implement ProtectedRoute for restrict access only for admins

---
+++ gemora/src/components/ProtectedRoute.tsx
@@ -0,0 +1,27 @@
+import React, {useContext} from 'react';
+import { Navigate } from 'react-router-dom';
+import {ACCESS_TOKEN} from "../constants/constants";
+import UserContext from "../context/UserContext";
+
+interface ProtectedRouteProps {
+    children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = (props) => {
+    const {getUserRole} = useContext(UserContext);
+    const jwt = localStorage.getItem(ACCESS_TOKEN);
+
+    if (!jwt) {
+        return <Navigate to={"/"} replace />;
+    }
+
+    const userRole = getUserRole();
+
+    if (userRole != "ADMIN") {
+        return <Navigate to={"/"} replace />;
+    }
+
+    return <>{props.children}</>;
+};
+
+export default ProtectedRoute;
