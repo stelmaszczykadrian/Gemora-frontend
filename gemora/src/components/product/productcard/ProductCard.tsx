d2fc6b132c9affc50c181d28e9f39d59c2493872
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-11-13 12:25:50 +0100

    GEM-63 Implement product deletion in admin profile

--- gemora/src/components/product/productcard/ProductCard.tsx
+++ gemora/src/components/product/productcard/ProductCard.tsx
@@ -1,15 +1,18 @@
 import React, {useEffect, useState} from "react";
 import './ProductCard.css';
 import {Product} from "../../../interfaces/ProductInterface";
-import {fetchAllProductsDataFromApiBySortType} from "../../../api/ProductApi";
+import {deleteProductById, fetchAllProductsDataFromApiBySortType} from "../../../api/ProductApi";
 import {SortType} from "../../../constants/constants";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPencil, faPlus, faTrash} from "@fortawesome/free-solid-svg-icons";
 import {Link, useNavigate} from "react-router-dom";
+import DeleteConfirmationModal from "../../ui/modals/deleteconfirmation/DeleteConfirmationModal";
 
 export default function ProductCard() {
     const [productList, setProductList] = useState<Product[]>([]);
     const navigate = useNavigate();
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+    const [productId, setProductId] = useState(0)
 
     useEffect(() => {
         fetchAllProductsDataFromApiBySortType(SortType.NEWEST)
@@ -25,6 +28,17 @@ export default function ProductCard() {
         navigate(`/products/${productId}`);
     };
 
+    const deleteOffer = async () => {
+        await deleteProductById(productId);
+        window.location.reload();
+        setIsDeleteModalOpen(false);
+    };
+
+    function handleDeleteOffer(id: number) {
+        setProductId(id);
+        setIsDeleteModalOpen(true);
+    }
+
 
     return (
         <div className="container d-flex justify-content-center product-card-container">
@@ -32,19 +46,21 @@ export default function ProductCard() {
                 <div className="container product-card-info">
                     <p className="product-card-total-products">Total products: <span className="product-list-length"> {productList.length}</span></p>
                     <p>
-                        <Link to="/product/management/add">
+                        <Link to="/products/management/add">
                             <FontAwesomeIcon icon={faPlus} className="product-card-plus-icon"/>
                         </Link>
                     </p>
                 </div>
-                {productList.map((product, index) => (
-                    <div className="product-card" onClick={() => handleProductClick(product.id)}>
-                        <p className="product-card-title">{product.name}</p>
+                {productList.map((product) => (
+                    <div className="product-card" key={product.id}>
+                        <p className="product-card-title" onClick={() => handleProductClick(product.id)}>{product.name}</p>
                         <p><FontAwesomeIcon icon={faPencil} className="product-item-icon"/></p>
-                        <p><FontAwesomeIcon icon={faTrash} className="product-item-icon"/></p>
+                        <p><FontAwesomeIcon icon={faTrash} className="product-item-icon" onClick={() => handleDeleteOffer(product.id)}/></p>
                     </div>
                 ))}
             </div>
+            <DeleteConfirmationModal isOpen={isDeleteModalOpen} handleCancel={() => setIsDeleteModalOpen(false)}
+                                     handleConfirm={deleteOffer}/>
         </div>
 
 
