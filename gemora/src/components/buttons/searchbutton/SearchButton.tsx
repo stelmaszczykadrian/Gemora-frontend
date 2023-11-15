d8e151e6f3e5937f67d8f47f8bfd4bddbd28d1a3
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-11-15 12:27:58 +0100

    GEM-48 Replace buttons with icons, optimize SVG handling in code, and move styles from the buttons package

--- gemora/src/components/buttons/searchbutton/SearchButton.tsx
+++ gemora/src/components/buttons/searchbutton/SearchButton.tsx
@@ -1,33 +1,19 @@
-import React, { FC, MouseEvent } from 'react';
+import React, {FC, MouseEvent} from 'react';
 import {Button} from "react-bootstrap";
-
+import MagnifierIcon from "../../icons/MagnifierIcon";
+import './SearchButton.css';
 
 interface SearchButtonProps {
     handleShow: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
-const SearchButton: FC<SearchButtonProps> = ({ handleShow }) => (
+const SearchButton: FC<SearchButtonProps> = ({handleShow}) => (
     <Button
-        style={{
-            width: '3rem',
-            height: '3rem',
-            position: 'relative',
-            marginLeft: 'auto',
-        }}
         variant="outline-light"
-        className="rounded-circle"
+        className="search-button rounded-circle"
         onClick={handleShow}
     >
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-        >
-            <path
-                d="M15.5 14h-.79l-.28-.27c1.26-1.51 2-3.39 2-5.46 0-4.42-3.58-8-8-8s-8 3.58-8 8 3.58 8 8 8c2.07 0 3.95-.74 5.46-2l.27.28v.79l5 4.99 1.28-1.28-4.99-5zm-7 0c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"
-            />
-            <path d="M0 0h24v24H0z" fill="none" />
-        </svg>
+        <MagnifierIcon/>
     </Button>
 );
 
