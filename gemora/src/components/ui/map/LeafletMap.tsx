1af69b854feacf80279a105b838e7b9f0b5f1a0e
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-11-17 16:01:00 +0100

    GEM-43 Add Leaflet Map feature in contact box

---
+++ gemora/src/components/ui/map/LeafletMap.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import './LeafletMap.css'
+
+const LeafletMap: React.FC = () => {
+    return (
+        <div className="map-container">
+            <MapContainer center={[52.2295, 21.0203]} zoom={15} style={{ height: '100%', width: '100%' }}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[52.2295, 21.0203]}>
+                    <Popup>Warsaw</Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
+};
+export default LeafletMap;
--- gemora/src/pages/Contact.tsx
+++ gemora/src/pages/Contact.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactBox from "../components/contact/ContactBox";
+import GemoraMap from "../components/ui/map/LeafletMap";
 
 const Contact = () => {
     return <>
         <ContactBox/>
+        <GemoraMap/>
     </>;
 };
 
