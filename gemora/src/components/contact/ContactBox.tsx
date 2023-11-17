c65b1c1d0a6070b07bb547950f1b144ffa9eebc7
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-11-17 16:02:45 +0100

    GEM-12 Update Contact Box UI

--- gemora/src/components/contact/ContactBox.tsx
+++ gemora/src/components/contact/ContactBox.tsx
@@ -1,141 +1,109 @@
-import {ErrorMessage, Field, Form, Formik, FormikProps} from "formik";
-import * as Yup from 'yup';
-import ContactCard from "./ContactCard";
-import './ContactBox.css';
+import {ErrorMessage, Field, Form, Formik, FormikHelpers} from "formik";
 import React from "react";
+import * as Yup from "yup";
+import {toast} from "react-toastify";
+import './ContactBox.css';
 
-interface ContactFormValues {
+export interface ContactFormValues {
     name: string;
     email: string;
     message: string;
 }
 
-const data = [
-    {
-        iconClassName: 'bx bxl-linkedin contact-card-icon',
-        title: 'LinkedIn',
-        link: 'https://linkedin.com/in/stelmaszczykadrian',
-        buttonText: 'LinkedIn profile',
-    },
-    {
-        iconClassName: 'bx bxl-github contact-card-icon',
-        title: 'GitHub',
-        link: 'https://github.com/stelmaszczykadrian',
-        buttonText: 'GitHub profile',
-    },
-    {
-        iconClassName: 'bx bx-mail-send contact-card-icon',
-        title: 'Email',
-        link: 'mailto:stelmaszczykadrian@gmail.com',
-        buttonText: 'Write me',
-    },
-];
-
-export default function ContactBox(){
-    const initialValues = {
+export default function ContactBox() {
+    const initialValues: ContactFormValues = {
         name: '',
         email: '',
-        message: '',
+        message: ""
     };
 
     const validationSchema = Yup.object().shape({
-        name: Yup.string()
-            .required('Name is required.')
-            .min(3, "Name too short.")
-            .max(20, "Name too long."),
+        name: Yup.string().required('Name is required.'),
         email: Yup.string().email('Invalid email').required('Email is required.'),
-        message: Yup.string()
-            .required('Message is required.')
-            .max(500, 'Max 500 characters.'),
+        message: Yup.string().required('Message is required'),
     });
 
-    const sendEmail = (e: React.MouseEvent, formik: FormikProps<ContactFormValues>) => {
-        console.log('test');
+    const handleSubmit = async (
+        values: ContactFormValues,
+        {setSubmitting}: FormikHelpers<ContactFormValues>
+    ) => {
+        try {
+
+        } catch (error) {
+            console.error('Error sending data to backend:', error);
+            toast.error('Something goes wrong.');
+        } finally {
+            setSubmitting(false);
+        }
     };
 
-    return(
-        <div className="contact-section">
-            <div className="container">
-                <div className="register-top-section">
-                    <h1 className="register-section-title">Contact Us</h1>
-                    <p className="register-divider"></p>
-                    <p className="register-txt">We are happy to help you with any questions you may have. Please do not hesitate to call or contact us via the enquiry form below. Alternatively if you would like to visit our showroom please click below to arrange an appointment.</p>
-                </div>
-            </div>
-            <div className="contact-info-container contact-container contact-grid">
-                <div className="contact-content">
-                    <div className="contact-info">
-                        {data.map(({ iconClassName, title, link, buttonText }, index) => (
-                            <ContactCard
-                                key={index}
-                                iconClassName={iconClassName}
-                                title={title}
-                                link={link}
-                                buttonText={buttonText}
-                            />
-                        ))}
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-lg-6 contact-container">
+                    <div className="contact-card-container">
+                        <div className="contact-form-container">
+                            <div className="contact-top-section">
+                                <h1 className="contact-section-title">Contact Us</h1>
+                                <p className="contact-divider"></p>
+                                <p className="contact-txt">If you have any questions or concerns, please contact us.</p>
+                            </div>
+                            <Formik
+                                initialValues={initialValues}
+                                validationSchema={validationSchema}
+                                onSubmit={handleSubmit}
+                            >
+                                <Form>
+                                    <div className="form-group">
+                                        <label htmlFor="name">Name</label>
+                                        <Field name="name" type="text" className="form-control contact-form-field" placeholder="Write your name"/>
+                                        <ErrorMessage
+                                            name="name"
+                                            component="div"
+                                            className="alert alert-danger contact-form-field"
+                                        />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="email">E-mail</label>
+                                        <Field
+                                            name="email"
+                                            type="email"
+                                            className="form-control contact-form-field"
+                                            placeholder="Write your e-mail"
+                                        />
+                                        <ErrorMessage
+                                            name="email"
+                                            component="div"
+                                            className="alert alert-danger contact-form-field"
+                                        />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="message">Message</label>
+                                        <Field
+                                            name="message"
+                                            // type="textarea"
+                                            as="textarea"
+                                            className="form-control contact-form-field contact-text-area"
+                                            placeholder="Write your message"
+                                        />
+                                        <ErrorMessage
+                                            name="message"
+                                            component="div"
+                                            className="alert alert-danger contact-form-field"
+                                        />
+                                    </div>
+                                    <div className="form-group text-center mt-4">
+                                        <button type="submit" className="contact-button">
+                                            Send message
+                                        </button>
+                                    </div>
+                                </Form>
+                            </Formik>
+                        </div>
                     </div>
                 </div>
-                <div className="contact-content">
-                    <Formik
-                        initialValues={initialValues}
-                        validationSchema={validationSchema}
-                        onSubmit={(values, { resetForm }) => {
-                            // Handle your form submission here, e.g., send an email
-                            console.log('Form values:', values);
-
-                            // Reset the form after successful submission
-                            resetForm();
-                        }}
-                    >
-                        <Form>
-                            <div className="contact-form">
-                                <div className="contact-form-div">
-                                    <label className="contact-form-tag" htmlFor="name">Name</label>
-                                    <Field name="name" type="text" className="contact-form-input" placeholder="Insert your name"/>
-                                    <ErrorMessage
-                                        name="name"
-                                        component="div"
-                                        className="error-message"
-                                    />
-                                </div>
-                            </div>
-                            <div className="contact-form">
-                                <div className="contact-form-div">
-                                    <label className="contact-form-tag" htmlFor="email">Email</label>
-                                    <Field name="email" type="email" className="contact-form-input" placeholder="Insert your email"/>
-                                    <ErrorMessage
-                                        name="email"
-                                        component="div"
-                                        className="error-message"
-                                    />
-                                </div>
-                            </div>
-                            <div className="contact-form">
-                                <div className="contact-form-div">
-                                    <label className="contact-form-tag" htmlFor="message">Message</label>
-                                    <Field
-                                        as="textarea"
-                                        name="message"
-                                        type="message"
-                                        className="contact-form-input custom-textarea"
-                                        placeholder="Write your message"
-                                    />
-                                    <ErrorMessage
-                                        name="message"
-                                        component="div"
-                                        className="error-message"
-                                    />
-                                </div>
-                            </div>
-                            <button type="submit" className="contact-button button-flex contact-button-send">
-                                {"Send message"}
-                                <i className={"uil uil-message contact-button-send-icon"}></i>
-                            </button>
-                        </Form>
-                    </Formik>
-                </div>
-
             </div>
         </div>
     );
