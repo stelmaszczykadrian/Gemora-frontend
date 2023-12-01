1261360a13bf36f9667749928af4e8d58fd578b0
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-12-01 12:47:29 +0100

    GEM-44: Display "No products" logo when products category not found

--- gemora/src/components/product/productslist/ProductList.tsx
+++ gemora/src/components/product/productslist/ProductList.tsx
@@ -3,29 +3,39 @@ import './ProductList.css';
 
 import {Link} from "react-router-dom";
 import ProductItem from "../productitem/ProductItem";
+import noProducts from "../../../assets/no-products.jpg";
 
 const ProductList: React.FC<{ products: any[] }> = ({ products }) => {
     return (
-        <div className="store-product-listing-container">
-            <div className="container">
-                <div className="store-products-grid">
-                    {products.map((product) => (
-                        <div key={product.id} className="container">
-                            <Link
-                                onClick={() => window.scrollTo(0, 0)}
-                                to={`/products/${product.id}`}
-                            >
-                                <ProductItem
-                                    key={product.id}
-                                    name={product.name}
-                                    price={product.price}
-                                    imageUrl={product.image}/>
-                            </Link>
-                        </div>
-                    ))}
+        <>
+            {products.length === 0 ? (
+                <div className="not-products-container">
+                    <img src={noProducts} alt="Not Found" className="not-product-image"/>
+                    <p className="not-products-message">Sorry there are already not products.</p>
                 </div>
-            </div>
-        </div>
+            ) : (
+                <div className="store-product-listing-container">
+                    <div className="container">
+                        <div className="store-products-grid">
+                            {products.map((product) => (
+                                <div key={product.id} className="container">
+                                    <Link
+                                        onClick={() => window.scrollTo(0, 0)}
+                                        to={`/products/${product.id}`}
+                                    >
+                                        <ProductItem
+                                            key={product.id}
+                                            name={product.name}
+                                            price={product.price}
+                                            imageUrl={product.image}/>
+                                    </Link>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
 
