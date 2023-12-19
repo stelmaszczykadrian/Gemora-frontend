44697128748238abe6eb1f1ecbc5079d5bbbc1d6
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-12-19 20:09:02 +0100

    GEM-132: Add .env file

---
+++ gemora/src/env.ts
@@ -0,0 +1,13 @@
+export const payUconfig = {
+    description: 'Gemora'
+}
+
+export const recaptchaConfig = {
+    REACT_APP_SITE_KEY: "6LeAlBUpAAAAAKnHeaDJ7_OqBy3NrH73gCCsxYvz",
+}
+
+export const emailJsConfig = {
+    SERVICE_ID: 'service_no3m1gl',
+    TEMPLATE_ID: 'template_nsewxeq',
+    PUBLIC_KEY: 'CUOSi8wL3BnG8nuZE'
+}
